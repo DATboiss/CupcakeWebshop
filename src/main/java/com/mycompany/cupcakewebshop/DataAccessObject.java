c58aa71b15de5b2bf5a26d1030fb11208925ce10
adamSai <adamsaidane@hotmail.com>
2018-02-28 11:01:16 +0100

    Created a getBottoms and getToppings to retrieve all the bottoms and toppings from the DB in a list.

--- src/main/java/com/mycompany/cupcakewebshop/DataAccessObject.java
+++ src/main/java/com/mycompany/cupcakewebshop/DataAccessObject.java
@@ -3,6 +3,7 @@ package com.mycompany.cupcakewebshop;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -51,6 +52,31 @@ public class DataAccessObject
         return bottom;
     }
 
+    public ArrayList<Bottom> getBottoms() throws Exception
+    {
+        Statement stmt = conn.getConnection().createStatement();
+        String sql = "select * from bottoms;";
+        ArrayList<Bottom> bottoms = new ArrayList<Bottom>();
+        Bottom bottom = null;
+        ResultSet rs = null;
+        try
+        {
+            rs = stmt.executeQuery(sql);
+            while (rs.next())
+            {
+                String name = rs.getString("name");
+                int price = rs.getInt("price");
+                bottom = new Bottom(name, price);
+                bottoms.add(bottom);
+
+            }
+        } catch (SQLException ex)
+        {
+            Logger.getLogger(DataAccessObject.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return bottoms;
+    }
+
     public Topping getTopping(String toppingName) throws SQLException
     {
         Statement stmt = conn.getConnection().createStatement();
@@ -64,6 +90,31 @@ public class DataAccessObject
             topping = new Topping(name, price);
         }
         return topping;
+    }
+
+    public ArrayList<Topping> getToppings() throws Exception
+    {
+        Statement stmt = conn.getConnection().createStatement();
+        String sql = "select * from toppings;";
+        ArrayList<Topping> toppings = new ArrayList<Topping>();
+        Topping topping = null;
+        ResultSet rs = null;
+        try
+        {
+            rs = stmt.executeQuery(sql);
+            while (rs.next())
+            {
+                String name = rs.getString("name");
+                int price = rs.getInt("price");
+                topping = new Topping(name, price);
+                toppings.add(topping);
+
+            }
+        } catch (SQLException ex)
+        {
+            Logger.getLogger(DataAccessObject.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return toppings;
     }
 
     public void newUser(String username, String pw, int balance) throws SQLException
