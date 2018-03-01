ab1fcf268c485f3b790c050ec23d5de4fc75e208
anderskruse <32634165+anderskruse@users.noreply.github.com>
2018-03-01 13:47:01 +0100

    added userInfo & stuff into user that was missing

--- src/main/java/Connector/DataAccessObject.java
+++ src/main/java/Connector/DataAccessObject.java
@@ -14,24 +14,20 @@ import java.util.logging.Logger;
  *
  * @author emilv
  */
-public class DataAccessObject
-{
+public class DataAccessObject {
 
     private final DBConnector conn;
 
-    public DataAccessObject() throws Exception
-    {
+    public DataAccessObject() throws Exception {
         this.conn = new DBConnector();
     }
 
-    public User getUser(String name) throws SQLException
-    {
+    public User getUser(String name) throws SQLException {
         Statement stmt = conn.getConnection().createStatement();
         String sql = "select * from login where email = " + "'" + name + "';";
         User user = null;
         ResultSet rs = stmt.executeQuery(sql);
-        if (rs.next())
-        {
+        if (rs.next()) {
             String username = rs.getString("email");
             String password = rs.getString("password");
             int balance = rs.getInt("balance");
@@ -40,14 +36,12 @@ public class DataAccessObject
         return user;
     }
 
-    public Bottom getBottom(String bottomName) throws SQLException
-    {
+    public Bottom getBottom(String bottomName) throws SQLException {
         Statement stmt = conn.getConnection().createStatement();
         String sql = "select * from bottoms where name = " + "'" + bottomName + "';";
         Bottom bottom = null;
         ResultSet rs = stmt.executeQuery(sql);
-        if (rs.next())
-        {
+        if (rs.next()) {
             String name = rs.getString("name");
             int price = rs.getInt("price");
             bottom = new Bottom(name, price);
@@ -55,39 +49,33 @@ public class DataAccessObject
         return bottom;
     }
 
-    public ArrayList<Bottom> getBottoms() throws Exception
-    {
+    public ArrayList<Bottom> getBottoms() throws Exception {
         Statement stmt = conn.getConnection().createStatement();
         String sql = "select * from bottoms;";
         ArrayList<Bottom> bottoms = new ArrayList();
         Bottom bottom = null;
         ResultSet rs = null;
-        try
-        {
+        try {
             rs = stmt.executeQuery(sql);
-            while (rs.next())
-            {
+            while (rs.next()) {
                 String name = rs.getString("name");
                 int price = rs.getInt("price");
                 bottom = new Bottom(name, price);
                 bottoms.add(bottom);
 
             }
-        } catch (SQLException ex)
-        {
+        } catch (SQLException ex) {
             Logger.getLogger(DataAccessObject.class.getName()).log(Level.SEVERE, null, ex);
         }
         return bottoms;
     }
 
-    public Topping getTopping(String toppingName) throws SQLException
-    {
+    public Topping getTopping(String toppingName) throws SQLException {
         Statement stmt = conn.getConnection().createStatement();
         String sql = "select * from toppings where name = " + "'" + toppingName + "';";
         Topping topping = null;
         ResultSet rs = stmt.executeQuery(sql);
-        if (rs.next())
-        {
+        if (rs.next()) {
             String name = rs.getString("name");
             int price = rs.getInt("price");
             topping = new Topping(name, price);
@@ -95,41 +83,47 @@ public class DataAccessObject
         return topping;
     }
 
-    public ArrayList<Topping> getToppings() throws Exception
-    {
+    public ArrayList<Topping> getToppings() throws Exception {
         Statement stmt = conn.getConnection().createStatement();
         String sql = "select * from toppings;";
         ArrayList<Topping> toppings = new ArrayList<Topping>();
         Topping topping = null;
         ResultSet rs = null;
-        try
-        {
+        try {
             rs = stmt.executeQuery(sql);
-            while (rs.next())
-            {
+            while (rs.next()) {
                 String name = rs.getString("name");
                 int price = rs.getInt("price");
                 topping = new Topping(name, price);
                 toppings.add(topping);
 
             }
-        } catch (SQLException ex)
-        {
+        } catch (SQLException ex) {
             Logger.getLogger(DataAccessObject.class.getName()).log(Level.SEVERE, null, ex);
         }
         return toppings;
     }
 
-    public void newUser(String username, String pw, int balance) throws SQLException
-    {
+    public void newUser(String email, String pw, int balance) throws SQLException {
         Statement stmt = conn.getConnection().createStatement();
-        String sql = "insert into login (email,password,balance) values ('" + username + "','" + pw + "'," + balance + ");";
-        try
-        {
+        String sql = "insert into login (email,password,balance) values ('" + email + "','" + pw + "'," + balance + ");";
+        try {
             stmt.executeUpdate(sql);
-        } catch (SQLException ex)
-        {
+        } catch (SQLException ex) {
             Logger.getLogger(DataAccessObject.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    public void userInfo(String email, String name, String address, int zip, int balance) throws SQLException {
+
+        Statement stmt = conn.getConnection().createStatement();
+        String sq1 = "insert into customerinfo (email, name, adresse, postalcode, balance) values ('" + email + "','" + name + "','" + address + "'," + zip + "," + balance + ");";
+            try {
+            stmt.executeUpdate(sq1);
+        } catch (SQLException ex) {
+            Logger.getLogger(DataAccessObject.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+            //help
     }
 }
--- src/main/java/Constructors/User.java
+++ src/main/java/Constructors/User.java
@@ -6,9 +6,13 @@ package Constructors;
  */
 public class User {
     private String email;
+    private String name;
     private String password;
     private int balance;
+    private String address;
+    private int zip; 
 
+  
     public User(String email, String password, int balance)
     {
         this.email = email;
